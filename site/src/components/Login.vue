be06351a27c7079e76bc8e33649cfe68dbed5d77
Matt <matt+gitkraken@artemisbot.uk>
2018-11-24 19:14:08 +0000

    Started site

---
+++ site/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="login">
+    <a href="#" class="btn"><MLHSVG class="logo"/> Login with MLH</a>
+  </div>
+</template>
+
+<script>
+import "spectre.css";
+import MLHSVG from "@/assets/mlh.svg";
+export default {
+  name: "Login",
+  components: {
+    MLHSVG
+  },
+  data() {
+    return {
+      url: `https://my.mlh.io/oauth/authorize?client_id=30d432a51dab16b788af5145e7c818a5154ebb282f93d6c862b2f3d4d71eb930&redirect_uri=${encodeURI(
+        window.location.host + "/login/callback"
+      )}&response_type=code&scope=phone_number+email+event`
+    };
+  }
+};
+</script>
+
+<style scoped>
+.logo {
+  width: 30px;
+  margin-right: 10px;
+}
+.btn {
+  display: inline-flex;
+  align-items: center;
+}
+</style>
--- site/src/main.js
+++ site/src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import { SimpleSVG } from "vue-simple-svg";
 
 Vue.config.productionTip = false;
+Vue.component("simple-svg", SimpleSVG);
 
 new Vue({
   router,
