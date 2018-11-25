1a12ea81e2f282f2c0ee7626a5ea3ff87d8c5e44
Liam <liamthorne4@gmail.com>
2018-11-25 10:54:03 +0000

    Added SMS App

---
+++ app.js
@@ -0,0 +1,23 @@
+/**Sends an SMS message to the specified recipient with information
+ * of a request from the speficied user
+ */
+function sendMessage (userName, recipient) {
+	// Create API object
+	const Nexmo = require('nexmo')
+	const nexmo = new Nexmo({
+	  apiKey: '***REMOVED***',
+	  apiSecret: '***REMOVED***'
+	})
+
+	// Message information
+	const from = 'Liam' // Set to the name of the group later
+	var text = userName + ' requested to join your hacking team'
+	
+	recipient = recipient.replace(/\s+/g, '')
+	recipient = recipient.replace(/\+/g, '');
+
+	nexmo.message.sendSms(from, recipient, text)
+}
+
+
+module.exports = sendMessage;
---
+++ test.js
@@ -0,0 +1,3 @@
+// Example of how call the method to send SMS
+const sms = require('./app.js');
+sms('paul', '***REMOVED***');
